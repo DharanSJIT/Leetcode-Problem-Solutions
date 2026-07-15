a3e631419995aa827a12a581328992146fc9c89f
Dharan M J <23it1212@stjosephstechnology.ac.in>
2026-07-15 22:43:13 +0530

    Time: 1 ms (99.97%), Space: 46.2 MB (79.92%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int n=ransomNote.length();
+        int m=magazine.length();
+        if(n>m) return false;
+        int[] freq=new int[26];
+        for(int i=0;i<m;i++){
+            freq[magazine.charAt(i)-'a']++;
+        }
+        for(int i=0;i<n;i++){
+            if(freq[ransomNote.charAt(i)-'a']==0){
+                return false;
+            }
+            freq[ransomNote.charAt(i)-'a']--;
+        }
+        return true;
+    }
+}
