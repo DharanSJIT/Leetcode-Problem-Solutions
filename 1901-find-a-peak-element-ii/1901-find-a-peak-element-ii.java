66c01f972dca12aed88b7ff48b8a1a064075f7aa
Dharan M J <23it1212@stjosephstechnology.ac.in>
2026-08-23 22:45:02 +0530

    Time: 5 ms (7.68%), Space: 113.9 MB (99.79%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -2,8 +2,8 @@ class Solution {
     public int[] findPeakGrid(int[][] mat) {
         int n=mat.length;
         int m=mat[0].length;
+        int dummy[][]=new int[n+2][m+2];
         ArrayList<Integer> res=new ArrayList<>();
-        int[][] dummy=new int[n+2][m+2];
         for(int i=0;i<n+2;i++){
             for(int j=0;j<m+2;j++){
                 dummy[i][j]=-1;
@@ -17,21 +17,16 @@ class Solution {
         for(int i=1;i<n+1;i++){
             for(int j=1;j<m+1;j++){
                 int temp=dummy[i][j];
-                if(temp>dummy[i+1][j] && temp>dummy[i-1][j] &&
-                temp>dummy[i][j-1]&&
-                temp>dummy[i][j+1]){
+                if(temp>dummy[i][j-1] && temp>dummy[i][j+1] && temp>dummy[i-1][j] && temp>dummy[i+1][j]){
                     res.add(i-1);
                     res.add(j-1);
                     break;
                 }
             }
         }
-        int[] ans=new int[2];
-        int i=0;
-        for(int num : res){
-            ans[0]=res.get(0);
-            ans[1]=res.get(1);
-        }
+        int []ans=new int[2];
+        ans[0]=res.get(0);
+        ans[1]=res.get(1);
         return ans;
     }
 }
